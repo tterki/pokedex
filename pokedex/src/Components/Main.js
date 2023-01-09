4077c0deca16663bbab8d7935bfe2cc9fd528490
Toufik Terki <terkitoufik@gmail.com>
2023-01-09 10:49:39 -0500

    commit pokedex

---
+++ pokedex/src/Components/Main.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from "react";
+import Card from "./Card";
+import Pokeinfo from "./Pokeinfo";
+import axios from "axios";
+
+const Main = () => {
+  // constants :
+  const [pokeData, setPokeData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
+  const [nextUrl, setNextUrl] = useState();
+  const [prevUrl, setPrevUrl] = useState();
+  const [pokeDex, setPokeDex] = useState();
+
+  //  functions :
+  const pokeFun = async () => {
+    setLoading(true);
+    const res = await axios.get(url);
+    setNextUrl(res.data.next);
+    setPrevUrl(res.data.previous);
+    getPokemon(res.data.results);
+    setLoading(false);
+    // console.log(pokeData)
+  };
+
+  const getPokemon = async (res) => {
+    res.map(async (item) => {
+      const result = await axios.get(item.url);
+      setPokeData((state) => {
+        state = [...state, result.data];
+        state.sort((a, b) => (a.id > b.id ? 1 : -1));
+        return state;
+      });
+      // console.log(result.data)
+    });
+  };
+
+  useEffect(() => {
+    pokeFun();
+  }, [url]);
+
+  // return :
+  return (
+    <>
+      <div className="container">
+        <div className="left-content">
+          <Card
+            pokemon={pokeData}
+            loading={loading}
+            infoPokemon={(poke) => setPokeDex(poke)}
+          />
+          <br></br>
+          <div className="btn-group">
+            {prevUrl && <button
+              onClick={() => {
+                setPokeData([]);
+                setUrl(prevUrl);
+              }}
+            >
+              Previous
+            </button>}
+
+            {nextUrl && <button
+              onClick={() => {
+                setPokeData([]);
+                setUrl(nextUrl);
+              }}
+            >
+              Next
+            </button>}
+          </div>
+        </div>
+        <div className="right-content">
+          <Pokeinfo data={pokeDex} />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
---
+++ pokedex/src/Components/Pokeinfo.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+const Pokeinfo = ({ data }) => {
+  // console.log(data);
+  return (
+    <>
+      {!data ? (
+        ""
+      ) : (
+        <>
+          <h1>{data.name}</h1>
+          <img
+            // src={data.sprites.front_default}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${data.id}.svg`}
+            alt={data.sprites.front_default}
+          ></img>
+
+          <div className="abilities">
+            {data.abilities.map((poke) => {
+              return (
+                <>
+                  <div className="group">
+                    <h2>{poke.ability.name}</h2>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+          <div className="base-stat">
+            {data.stats.map((poke) => {
+              return (
+                <>
+                  <h3>
+                    {poke.stat.name}:{poke.base_stat}
+                  </h3>
+                </>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Pokeinfo;
